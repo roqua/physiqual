0ab1e6b3bd351793232a790b84d53695f9c2e8e3
Frank Blaauw <frank.blaauw@gmail.com>
2015-09-08 14:37:13 +0200

    Added bucket generators

---
+++ lib/bucket_generators/bucket_generator.rb
@@ -0,0 +1,15 @@
+module BucketGenerators
+  module BucketGenerator
+    def generate(from, to)
+      fail 'Not implemented by subclass'
+    end
+
+    # TODO: This is copied from dataservice. Remove it here and find a better place to store it.
+    def output_entry(date, values)
+      {
+        DataServices::DataService::DATE_TIME_FIELD => date,
+        DataServices::DataService::VALUES_FIELD => [values].flatten
+      }
+    end
+  end
+end
