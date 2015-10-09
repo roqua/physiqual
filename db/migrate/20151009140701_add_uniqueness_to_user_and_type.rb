0dcd2a9c7ab2071ca115317cb0ed6bf8443e5bdf
Frank Blaauw <frank.blaauw@gmail.com>
2015-10-09 16:12:28 +0200

    Added uniqueness to db

---
+++ db/migrate/20151009140701_add_uniqueness_to_user_and_type.rb
@@ -0,0 +1,9 @@
+class AddUniquenessToUserAndType < ActiveRecord::Migration
+  def up
+    add_index :physiqual_tokens, [:type, :physiqual_user_id], unique: true
+  end
+
+  def down
+    remove_index :physiqual_tokens, [:type, :physiqual_user_id], unique: true
+  end
+end
