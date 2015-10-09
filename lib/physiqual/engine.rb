668454df2f7cd6174512b806b7a9fd347ef78677
frbl <frank.blaauw@gmail.com>
2015-10-09 15:06:03 +0200

    Added initializer for migrations

--- lib/physiqual/engine.rb
+++ lib/physiqual/engine.rb
@@ -20,6 +20,14 @@ module Physiqual
   class Engine < ::Rails::Engine
     isolate_namespace Physiqual
 
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
+
     config.generators do |g|
       g.test_framework :rspec, fixture: false
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
