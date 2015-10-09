9adba6dc396c6a8986ee4300bd5bcf5e743b3666
Frank Blaauw <frank.blaauw@gmail.com>
2015-10-09 15:52:18 +0200

    Fixed rubocop

--- lib/physiqual/engine.rb
+++ lib/physiqual/engine.rb
@@ -22,8 +22,8 @@ module Physiqual
 
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
-        config.paths["db/migrate"].expanded.each do |expanded_path|
-          app.config.paths["db/migrate"] << expanded_path
+        config.paths['db/migrate'].expanded.each do |expanded_path|
+          app.config.paths['db/migrate'] << expanded_path
         end
       end
     end
