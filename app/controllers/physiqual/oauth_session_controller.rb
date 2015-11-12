ef6d5508045eeb2721e47160e5ca23defe896a71
Frank Blaauw <frank.blaauw@gmail.com>
2015-11-12 13:19:30 +0100

    Removed more commented code

--- app/controllers/physiqual/oauth_session_controller.rb
+++ app/controllers/physiqual/oauth_session_controller.rb
@@ -16,20 +16,9 @@ module Physiqual
       end
       # session = Sessions::TokenAuthorizedSession.new(current_user.google_tokens.first.token, GoogleToken.base_uri)
       # session = Sessions::TokenAuthorizedSession.new(current_user.fitbit_tokens.first.token, FitbitToken.base_uri)
+      
       # render json: DataServices::GoogleService.new(session).steps(from, to) and return
-      # render json: DataServices::GoogleService.new(session).distance(from, to) and return
-      # render json: DataServices::GoogleService.new(session).sleep(20.days.ago.beginning_of_day.in_time_zone,
-      # Time.zone.now) and return
-      # render json: DataServices::GoogleService.new(session).sources and return
-      # render json: DataServices::GoogleService.new(session).calories(from, to) and return
       # render json: DataServices::FitbitService.new(session).calories(from, to) and return
-      # last_measurement_time = Time.now.change(hour: 22, min: 00)
-      # measurements_per_day = 3
-      # interval = 6
-      # service = DataServices::FitbitService.new(current_user.fitbit_tokens.first)
-      # render json: service.heart_rate(from, to) and return
-      # render json: DataServices::SummarizedDataService.new(service,
-      # last_measurement_time, measurements_per_day, interval, false).steps(from, to) and return
 
       number_of_days = 30
       respond_to do |format|
