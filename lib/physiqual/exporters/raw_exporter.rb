9678b9dcc0beb1f870bbe7bfd1c4a72c88bb4cd4
Ando Emerencia <ando.emerencia@gmail.com>
2015-11-12 14:11:26 +0100

    codeclimate

--- lib/physiqual/exporters/raw_exporter.rb
+++ lib/physiqual/exporters/raw_exporter.rb
@@ -13,17 +13,22 @@ module Physiqual
         token = tokens.first
         session = Sessions::TokenAuthorizedSession.new(token.token, token.class.base_uri)
         service = DataServices::DataServiceFactory.fabricate!(token.class.csrf_token, session)
-
-        from = first_measurement - Physiqual.hours_before_first_measurement.hours
-        to   = first_measurement + (number_of_days - 1).days +
-               ((Physiqual.measurements_per_day - 1) * Physiqual.interval).hours
-
+        from, to = determine_time_span(first_measurement, number_of_days)
         service.send(@data_source.to_sym, from, to)
       end
 
       def export(user, first_measurement, number_of_days)
         result = export_data(user, first_measurement, number_of_days)
         result.to_json
+      end
+
+      private
+
+      def determine_time_span
+        from = first_measurement - Physiqual.hours_before_first_measurement.hours
+        to = first_measurement + (number_of_days - 1).days +
+             ((Physiqual.measurements_per_day - 1) * Physiqual.interval).hours
+        [from, to]
       end
     end
   end
