19fcae15aaad1d98233a2a95657af46596b37731
Frank Blaauw <frank.blaauw@gmail.com>
2015-12-02 11:13:21 +0100

    Renamed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Physiqual::Engine.routes.draw do
   resources :exports, only: [:index]
-  get 'exports/providers/:provider/data_source/:data_source', :to => 'exports#raw'
+  get 'exports/providers/:provider/data_sources/:data_source', :to => 'exports#raw'
 
   get 'auth/:provider/authorize', :to => 'sessions#authorize'
   get 'auth/:provider/callback',  :to => 'sessions#create'
