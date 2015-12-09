9fc0d2543240acfbd7cbbdd86a14896d41ff3930
Ando Emerencia <ando.emerencia@gmail.com>
2015-12-09 15:21:52 +0100

    rubocop

--- lib/physiqual/omniauth/strategies.rb
+++ lib/physiqual/omniauth/strategies.rb
@@ -1,15 +1,15 @@
-module OmniAuth::Strategies
+module OmniAuth
+  module Strategies
+    class PhysiqualFitbitOauth2 < FitbitOauth2
+      def name
+        :physiqual_fitbit_oauth2
+      end
+    end
 
-  class PhysiqualFitbitOauth2 < FitbitOauth2
-    def name
-      :physiqual_fitbit_oauth2
+    class PhysiqualGoogleOauth2 < GoogleOauth2
+      def name
+        :physiqual_google_oauth2
+      end
     end
   end
-
-  class PhysiqualGoogleOauth2 < GoogleOauth2
-    def name
-      :physiqual_google_oauth2
-    end
-  end
-
 end
