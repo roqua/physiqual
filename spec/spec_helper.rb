761c0cf4fd76b738e0da09f33d152453d5d1674d
Ando Emerencia <ando.emerencia@gmail.com>
2016-01-06 14:12:53 +0100

    rubocop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -82,7 +82,6 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   # config.order = "random"
 
-
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
