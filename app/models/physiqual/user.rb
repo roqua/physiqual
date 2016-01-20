003b5ac40d3de6d5be54c4f90527db8c1e69abde
Ando Emerencia <ando.emerencia@gmail.com>
2016-01-20 16:44:37 +0100

    update token creation functions

--- app/models/physiqual/user.rb
+++ app/models/physiqual/user.rb
@@ -5,9 +5,5 @@ module Physiqual
     has_one :physiqual_token, foreign_key: 'physiqual_user_id', class_name: 'Physiqual::Token'
     validates :user_id, presence: true
 
-    def find_tokens(csrf_token)
-      return nil unless csrf_token
-      tokens.select { |x| x.class.csrf_token == csrf_token }
-    end
   end
 end
