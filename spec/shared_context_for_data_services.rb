6f6118d3eb623c7c7e67c2288c0642bef99ff3dd
Frank Blaauw <frank.blaauw@gmail.com>
2016-01-25 11:47:32 +0100

    Fixed general context specs for data services

--- spec/shared_context_for_data_services.rb
+++ spec/shared_context_for_data_services.rb
@@ -7,18 +7,17 @@ module Physiqual
         expect(result).to be_a Array
         return true if result.nil?
         result.each do |entry|
-          expect(entry).to be_a Hash
-          expect(entry.keys.length).to eq 2
-          expect(entry.keys).to include DataService.new.date_time_field
-          expect(entry.keys).to include DataService.new.values_field
-          expect(entry[DataService.new.date_time_field]).to be_a Time
-          expect(entry[DataService.new.values_field]).to be_a Array
-          expect(entry[DataService.new.values_field].any? { |x| x.is_a? Array }).to be_falsey
+          expect(entry).to be_a DataEntry
+          expect(entry.start_date).to be_a Time
+          expect(entry.end_date).to be_a Time
+          expect(entry.measurement_moment).to be_a Time
+          expect(entry.values).to be_a Array
+          expect(entry.values.any? { |x| x.is_a? Array }).to be_falsey
         end
       end
 
       def check_start_end_date(result, from, to)
-        dates = result.map { |x| x[DataService.new.date_time_field] }
+        dates = result.map { |x| x.measurement_moment }
         lowest_date = dates.min
         highest_date = dates.max
         expect(lowest_date).to be_between(from.beginning_of_day, to.end_of_day)
