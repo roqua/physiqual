dd28e003a54114b907057daca9eb31e585b49bc9
Frank Blaauw <frank.blaauw@gmail.com>
2016-01-25 10:56:06 +0100

    Added module to data entry object and added defaults

--- app/models/physiqual/data_entry.rb
+++ app/models/physiqual/data_entry.rb
@@ -1,10 +1,17 @@
-class DataEntry
-  include Virtus.model
+module Physiqual
+  class DataEntry
+    include Virtus.model
 
-  DATE_FORMAT = '%Y-%m-%d'
+    DATE_FORMAT = '%Y-%m-%d'
 
-  attribute :start_date, DateTime
-  attribute :end_date, DateTime
-  attribute :values, Array
-  attribute :measurement_moment, DateTime
+    attribute :start_date, DateTime
+    attribute :end_date, DateTime
+    attribute :values, Array, default: []
+    attribute :measurement_moment, DateTime, default: default_measurement_moment
+
+    def default_measurement_moment
+      return nil if start_date.nil? || end_date.nil?
+      Time.at((start_date + end_date) / 2)
+    end
+  end
 end
