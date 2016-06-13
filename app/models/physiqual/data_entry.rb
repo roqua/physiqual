ace818028a5d79944fedd04346ea25ba2767ddd4
frbl <frank.blaauw@gmail.com>
2016-06-13 16:34:15 +0200

    remove trailing whitespacing

--- app/models/physiqual/data_entry.rb
+++ app/models/physiqual/data_entry.rb
@@ -4,18 +4,18 @@ module Physiqual
 
     DATE_FORMAT = '%Y-%m-%d'.freeze
 
-    # The start date property is the start of this dataentry object, meaning it 
+    # The start date property is the start of this dataentry object, meaning it
     # denotes the actual start of the data in this measurement
     attribute :start_date, DateTime
     
-    # The end date property denotes the end of this measurement (exclusing the time). 
+    # The end date property denotes the end of this measurement (exclusing the time).
     # Meaning, data in this data entry is upto, but not including this time point
     attribute :end_date, DateTime
     attribute :values, Array, default: []
     
-    # Measurement moment is used in the physiqual process. It is used to determine 
-    # the actual 'measurement time' of the current dataentry object. Often it's 
-    # precisely in the middle of the start and end date of this data entry object. 
+    # Measurement moment is used in the physiqual process. It is used to determine
+    # the actual 'measurement time' of the current dataentry object. Often it's
+    # precisely in the middle of the start and end date of this data entry object.
     attribute :measurement_moment, DateTime, default: :default_measurement_moment
 
     def default_measurement_moment
